bd7a4971989f84899c803a87ff2146507e39c74a
zhengminghui <zhengminghui@jd.com>
2020-04-18 21:40:57 +0800

    first commit for version 1.0.0

---
+++ Example/Pods/Target Support Files/MyLib/MyLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyLib : NSObject
+@end
+@implementation PodsDummy_MyLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyLib_Example/Pods-MyLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MyLib_Example
+@end
